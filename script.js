9969e915ba0a3ddda1e4606f1d8a600f4ef1584b
easytigers <104019934+easytigers@users.noreply.github.com>
2022-04-21 15:31:34 -0400

    updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Write your JavaScript code here!
 
-const { myFetch, pickPlanet, addDestinationInfo } = require("./scriptHelper");
+// const { myFetch, pickPlanet, addDestinationInfo } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
 
@@ -15,7 +15,7 @@ window.addEventListener("load", function() {
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
         let planet = pickPlanet(listedPlanets);
         console.log(planet);
-        addDestinationInfo(document,planet.name,planet.diameter,planet.star,planet.distance,planet.moons,planet.imageUrl);
+        addDestinationInfo(document,planet.name,planet.diameter,planet.star,planet.distance,planet.moons,planet.image);
     })
     
     
